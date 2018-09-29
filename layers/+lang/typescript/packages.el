3d2914076b90a5cb8cd89a7c2af8b50669e91c0a
Tommi Komulainen <tommi.komulainen@iki.fi>
2018-09-29 20:27:18 +0200

    typescript: add-node-modules-path -> node-executable-find

--- layers/+lang/typescript/packages.el
+++ layers/+lang/typescript/packages.el
@@ -11,7 +11,6 @@
 
 (setq typescript-packages
       '(
-        add-node-modules-path
         company
         eldoc
         emmet-mode
@@ -23,10 +22,6 @@
         tide
         yasnippet
         ))
-
-(defun typescript/post-init-add-node-modules-path ()
-  (spacemacs/add-to-hooks #'add-node-modules-path '(typescript-mode-hook
-                                                    typescript-tsx-mode-hook)))
 
 (defun typescript/post-init-company ()
   (spacemacs/add-to-hooks #'spacemacs//typescript-setup-company
